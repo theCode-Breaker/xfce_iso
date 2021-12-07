bfd9d9135563673aaad6dbabc690a940426a1d9c
TechXero <steve@techxero.com>
2021-12-07 19:49:21 +0200

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# xerolinux_xfce_iso
-XeroLinux XFCE ISO Repo
+# XeroLinux Pure ISO Repo
+
+Repo for **XeroLinux XFCE** ISO. Feel free to go through files and learn how it'ss all done. **DO NOT BUILD** with it as is, it will not work, modify it, then build. ;)
+
+![XeroLinux-Calamares](https://i.imgur.com/9sjGFSN.png)
---
+++ archiso/profiledef.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="xerolinux"
+iso_label="XEROLINUX_XFCE"
+iso_publisher="XeroLinux <https://xerolinux.online>"
+iso_application="XeroLinux Live/Rescue CD"
+iso_version="xfce"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/vcheck"]="0:0:755"
+  ["/usr/local/bin/reset"]="0:0:755"
+  ["/usr/local/bin/vbox-share"]="0:0:755"
+  ["/usr/local/bin/dmcheck"]="0:0:755"
+  ["/usr/local/bin/xerolinux-before"]="0:0:755"
+  ["/usr/local/bin/skel"]="0:0:755"
+  ["/usr/local/bin/all-cores"]="0:0:755"
+  ["/usr/local/bin/keyfix"]="0:0:755"
+  ["/usr/local/bin/xerolinux-final"]="0:0:755"
+  ["/usr/local/bin/services"]="0:0:755"
+  ["/usr/local/bin/remove-nvidia"]="0:0:755"
+  ["/usr/local/bin/welcome"]="0:0:755"
+  ["/usr/local/bin/grubfix"]="0:0:755"
+  )
